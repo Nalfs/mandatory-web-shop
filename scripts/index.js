5b09dbb4f5ae4fa049f3bfa4ec14006b4b402473
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2018-02-18 19:35:08 +0100

    Fixed Zip and Adress validation misstake

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ let uLastname = document.getElementById("efternamn");
 let uEmail = document.getElementById("email");
 let uStreetAdress = document.getElementById("gatuadress");
 let uCity = document.getElementById("stad");
+let uzipCode = document.getElementById("zip");
 let btnSubmit = document.getElementById("btnSubmit");
 
 
@@ -14,7 +15,9 @@ btnSubmit.addEventListener("click",function () {
     let errEmail = document.getElementById("errEmail");
     let errGat = document.getElementById("errGat");
     let errCity = document.getElementById("errCity");
+    let errZip = document.getElementById("errZip");
     let emailPattern = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+    let zipPattern = /^[a-zA-Z0-9\s,'-]+$/;
 
     if (uFirstname.value === '' || uFirstname.value.length < 3){
         errName.setAttribute("style", "visibility:visible");
@@ -25,12 +28,14 @@ btnSubmit.addEventListener("click",function () {
     }if((uEmail.value === '' || uEmail.value.length < 5 || !emailPattern.test(uEmail.value))){
         errEmail.setAttribute("style", "visibility:visible");
 
-    }if(uStreetAdress.value === '' || uStreetAdress.value.length < 4) {
-        alert("Please input number");
+    }if((uStreetAdress.value === '' || uStreetAdress.value.length < 4 || !zipPattern.test(uStreetAdress.value))) {
         errGat.setAttribute("style", "visibility:visible");
 
-    }if(uCity.value === '' || uCity.value.length < 3){
-            errCity.setAttribute("style", "visibility:visible");
+    }if(uCity.value === '' || uCity.value.length < 3) {
+        errCity.setAttribute("style", "visibility:visible");
+
+    }if((uzipCode.value === '' || uzipCode.value.length < 3 || !zipPattern.test(uzipCode.value))){
+            errZip.setAttribute("style", "visibility:visible");
 
     }else{
         document.getElementById("frmUserInfo").submit();
