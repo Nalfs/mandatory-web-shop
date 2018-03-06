a83be6364f1fe67152b1425786be84da45e079ff
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2018-03-06 18:45:24 +0100

    fixed comment function

--- scripts/product-1.js
+++ scripts/product-1.js
@@ -1,36 +1,66 @@
 let section = document.getElementById('commentSection');
 let input = document.getElementById('name');
 let comment = document.getElementById('comment');
-let placeholderArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-
-//let placeholderArray = [];
-
-localStorage.setItem('items', JSON.stringify(placeholderArray));
-const data = JSON.parse(localStorage.getItem('items'));
 
 
+//saving text-section
 const addComments = (name, text) => {
     const article = document.createElement('article');
     article.textContent =  name + ' ' + text;
     section.appendChild(article);
 }
 
+
+
+//Definition for an object
+function Comment(txtName,txtComment){
+    this.name = txtName;
+    this.comment = txtComment;
+}
+
+let arrComments = [];
+
 $("#btnSubmit").click(function () {
+    //Create an object comment
+    let newComment = new Comment(input.value,comment.value);
 
-    placeholderArray.push(input.value, comment.value);
-    localStorage.setItem('items', JSON.stringify(placeholderArray));
+    //push in an object array
+    arrComments.push(newComment);
 
-    addComments(input.value, comment.value);
-    comment.value = "";
-    input.value = "";
 
+    //convert to JSON string
+    let sJSONComments = JSON.stringify(arrComments);
+
+
+    //save to sessionStorage
+    localStorage.wsComments = sJSONComments;
+
+    //retrieve comments from sessionStorage
+    giveComment();
 });
 
 
-data.forEach(item => {
-    addComments(item);
-});
+function giveComment() {
+    //get data from sessionStorage
+    let sJSONComments = localStorage.getItem("wsComments");
+    console.log("get back"+sJSONComments);
 
+    //convert from JSON string to object array
+    let data =  JSON.parse(localStorage.getItem("wsComments"));
+
+
+    //show array's content
+    let i;
+    let arrLength = data.length;
+    for (i = 0; i < arrLength; i++){
+        addComments(data[i].name, data[i].comment);
+        comment.value = "";
+        input.value = "";
+    }
+}
+
+
+/*//super-secret master-click-remover
 $("#btnDel").click(function () {
 
     localStorage.clear();
@@ -39,3 +69,4 @@ $("#btnDel").click(function () {
     }
 
 });
+*/
