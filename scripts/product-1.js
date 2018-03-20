41797fdd60447ac195a4793cfe959a2e35cc9b21
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2018-03-20 15:57:55 +0100

    reviews with rating added

--- scripts/product-1.js
+++ scripts/product-1.js
@@ -1,38 +1,82 @@
 let section = document.getElementById('commentSection');
 let input = document.getElementById('name');
 let comment = document.getElementById('comment');
+//let rating = document.getElementById('score');
+let rating = document.getElementsByName("score");
 
 //keep comments loaded
 let comments = JSON.parse(localStorage.getItem("wsComments")) || [];
 
-//Loop through localstorage
+//Loop through localstorage, render views
 for(let comment of comments) {
     const article = document.createElement('article');
-    article.textContent =  comment.name + ' ' + comment.comment;
+    let src = "";
+    switch(comment.rating){
+        case "1": src = "../views/images/1stars.png";
+            break;
+        case "2": src = "../views/images/2stars.png";
+            break;
+        case "3": src = "../views/images/3stars.png";
+            break;
+        case "4": src = "../views/images/4stars.png";
+            break;
+        case "5": src = "../views/images/5stars.png";
+            break;
+        default: console.log("default")
+    }
+    let image = document.createElement("img");
+    image.setAttribute("src", src);
+    console.log(comment.rating);
+    article.innerHTML =  '<h3 class="comment-name">' + comment.name + '</h3>' + ' ' + comment.comment;
+    article.appendChild(image);
     section.appendChild(article);
 }
 
 
 //saving text-section
-const addComments = (name, text) => {
+const addComments = (name, text, rating) => {
     const article = document.createElement('article');
-    article.textContent =  name + ' ' + text;
+    let src = "";
+    switch(rating){
+        case "1": src = "../views/images/1stars.png";
+            break;
+        case "2": src = "../views/images/2stars.png";
+        break;
+        case "3": src = "../views/images/3stars.png";
+            break;
+        case "4": src = "../views/images/4stars.png";
+            break;
+        case "5": src = "../views/images/5stars.png";
+            break;
+        default: console.log("default")
+    }
+    let image = document.createElement("img");
+    image.setAttribute("src", src);
+    article.innerHTML =  '<h3 class="comment-name">' + name + '</h3>' + ' ' + text;
+    article.appendChild(image)
     section.appendChild(article);
 };
 
 
 
 //Definition for an object
-function Comment(txtName,txtComment){
+function Comment(txtName,txtComment, rating){
     this.name = txtName;
     this.comment = txtComment;
+    this.rating = rating;
 }
 
 
 
 $("#btnSubmit").click(function () {
+    let newComment = {};
+    for (let i = 0, length = rating.length; i < length; i++) {
+        if (rating[i].checked) {
+            newComment = new Comment(input.value,comment.value, rating[i].value);
+            break;
+        }
+    }
     //Create an object comment
-    let newComment = new Comment(input.value,comment.value);
 
     //push in an object array
     comments.push(newComment);
@@ -62,20 +106,10 @@ function giveComment() {
     let i;
     let arrLength = data.length;
     for (i = 0; i < arrLength; i++){
-        addComments(data[i].name, data[i].comment);
+        addComments(data[i].name, data[i].comment , data[i].rating);
         comment.value = "";
         input.value = "";
+        rating.value = "";
     }
 }
 
-
-/*//super-secret master-click-remover
-$("#btnDel").click(function () {
-
-    localStorage.clear();
-    while (section.firstChild) {
-        section.removeChild(section.firstChild);
-    }
-
-});
-*/
